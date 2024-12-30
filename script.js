2941b2c3a4dca2f4701874206c8a6b7432fbbb46
Gogon <81472719+gogon290@users.noreply.github.com>
2024-12-30 23:04:30 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const button = document.getElementById('surpriseButton');
+
+  button.addEventListener('click', () => {
+    alert('Hello! Welcome to this simple website.');
+  });
+});
